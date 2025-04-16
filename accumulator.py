8e4705bec732e52d60c5b06653264ae71fb2fda3
yhc999 <2211824688@qq.com>
2025-04-16 20:35:42 +0800

    调整accumulator

--- accumulator.py
+++ accumulator.py
@@ -2,36 +2,44 @@
 通用积累器
 item: procedure,用来计算第i项
 combine: procedure,用来定义不同项之间结合的方式
-k: 起始值
-n: i的最终值
-start: i的初始值,也就是从第几项开始计算
+start: 累积的初始值, 如果是相加累积为0, 相乘为1
+n:  终止的项
+i:  开始的项, 默认为0
 
 example:
 
 计算 1 + 2 + 3 + ... + n: 
 
-常规写法:
-define item(i):
-    return i
-define combine(x, y):
-    return x + y
-define sum_i(n):
-    return accumulator(item, combine, 0, n, 1)
-
-lambda版本(语法糖,更加简洁):
 def sum_i(n):
     return accumulator(
         lambda i : i,
-        lambda x,y : x + y,
+        add,
         0,
         n,
         1
     )
 """
-def accumulator(item, combine, k, n, start):
-    i = start
+def accumulator(item, combine, start, n, i=0):
+    acc = start
     while i <= n:
         val = item(i) #第i项的值
-        k = combine(k, val)
+        acc = combine(acc, val)
         i += 1
-    return k
+    return acc
+
+#+
+def add(x, y):
+    return x + y
+
+#-
+def sub(x, y):
+    return x - y
+
+#*
+def mul(x, y):
+    return x * y    
+
+#/
+def div(x, y):
+    return x / y    
+
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from accumulator import accumulator
+from accumulator import *
 
 """
 下面所有的例子都会在前面给出一个要计算的序列和对应序列的通项公式, 计算的其实就是这个通项公式的n项积累,
@@ -10,13 +10,14 @@ from accumulator import accumulator
 def sum_i(n):
     return accumulator(
         lambda i : i,
-        lambda x,y : x + y,
+        add,
         0,
         n,
         1
     )
 # test
 # print(sum_i(3))
+# print(sum_i(4))
 # print(sum_i(10))
 # print(sum_i(100))
 # print(sum_i(1000))
@@ -27,7 +28,7 @@ def sum_i(n):
 def sum_2i(n):
     return accumulator(
         lambda i : pow(2, i),   # item
-        lambda x, y : x + y,     # combine
+        add,     # combine
         0,              
         n,
         0
@@ -35,6 +36,25 @@ def sum_2i(n):
 # test
 # print(sum_2i(2)) # 1 + 2 + 4 = 7
 # print(sum_2i(3)) # 1 + 2 + 4 + 8 = 15
-# print(sum_2i(4))
+# print(sum_2i(4)) # 31
 
-
+# target : 0 + 1 + 1 + 2 + 3 + 5 + 8 + 13 + 21 + ...(fibonacci)
+def fib(n):
+    if n <= 1:
+        return n
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
+def sum_fib(n):
+    return accumulator(
+        fib,
+        add,
+        0,
+        n
+    )
+# test
+# print(sum_fib(1))
+# print(sum_fib(2))
+# print(sum_fib(3))
+# print(sum_fib(4))
